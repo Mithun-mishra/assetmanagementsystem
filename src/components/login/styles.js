44dfc0076e5849fbd432c35e3576bacebe85e7f7
Mithun Mishra <mithunkumarmishra2020@gmail.com>
2023-09-04 16:32:43 +0530

    loginpage update

---
+++ src/components/login/styles.js
@@ -0,0 +1,50 @@
+export const styles = {
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "white",
+        py: "20px",
+        position: "relative",
+        marginTop:"5rem",
+        minWidth: "80%"
+      },
+    divider: {
+        minHeight:"2rem",
+        // backgroundColor:"blue",
+        backgroundColor:'rgb(13, 110, 253)',
+        // backgroundColor:"rgba(var(--bs-primary-rgb),var(--bs-bg-opacity))!important",
+        minHeight: "18px",
+        minWidth: "100%"
+      },
+    head: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        // fontFamily: "'Open Sans', sans-serif",
+        fontSize: "2.5rem",
+        fontWeight:"bold"
+    // color: "#2D2D2D",
+    // "@media (max-width: 650px)": {
+    //   fontSize: "28px",
+    // },
+},
+loginBox:{
+    // display: "flex",
+    // flexDirection: "column",
+    // alignItems: "center",
+    // justifyContent: "center",
+    minWidth: "40%"
+},
+userIdtext:{
+    textAlign: 'left',
+},
+ footerStyle:{
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#7dbcea',
+  },
+}
+
