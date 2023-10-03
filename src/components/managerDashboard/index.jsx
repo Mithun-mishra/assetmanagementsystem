10716c97e8e1694e1a27ae51acfd1f27828f7494
Mithun Mishra <mithunkumarmishra2020@gmail.com>
2023-10-03 21:10:49 +0530

    css done

--- src/components/managerDashboard/index.jsx
+++ src/components/managerDashboard/index.jsx
@@ -1,45 +1,47 @@
-import React from 'react'
-import { styles } from './styles'
-import { Typography } from '@mui/material'
-import { Divider } from 'antd';
-import Container from '@mui/material/Container';
-import FooterTab from '../footer';
-import NavTab from '../navbar';
-import DateAndTime from '../datatime';
-// import GridReport from '../Page/Grid Report';
-// import History from '../Page/History/Index';
-// import Reports from '../Page/Reports';
-// import FeedbackReports from '../Page/Feedback Reports';
+import React from "react";
+import { styles } from "./styles";
+import { Typography } from "@mui/material";
+import { Divider } from "antd";
+import Container from "@mui/material/Container";
+import FooterTab from "../footer";
+import NavTab from "../navbar";
+import DateAndTime from "../datatime";
+import GridReport from '../Page/Grid Report';
+import History from '../Page/History/Index';
+import Reports from '../Page/Reports';
+import FeedbackReports from '../Page/Feedback Reports';
 import PerformanceReport from '../Page/Performance Report';
-import ToiletList from '../Page/ToiletList';
-import UserList from '../Page/UserList';
+import ToiletList from "../Page/ToiletList";
+import UserList from "../Page/UserList";
+import AssetList from "../Page/AssetList";
 export default function ManagerDashboard() {
   return (
     <>
       <Container maxWidth="sm" sx={styles.container}>
-        <Typography sx={styles.head}>Asset Management System - Kalinganagar </Typography>
+        <Typography sx={styles.head}>
+          Asset Management System - Kalinganagar
+        </Typography>
         <NavTab />
-        <div >
+        <div>
           <DateAndTime />
         </div>
-        <Divider style={{ border: '0.10px solid #C7C7C8' }} />
+        <Divider style={{ border: "0.10px solid #C7C7C8" }} />
 
-        <div>
+        <div style={{width:'100%'}}>
           {/* <GridReport /> */}
           {/* <History /> */}
-          {/* <Reports /> */}
+          <Reports />
           {/* <FeedbackReports /> */}
-          <PerformanceReport />
-        {/* <ToiletList/> */}
- <UserList/>
-            </div>
+          {/* <PerformanceReport /> */}
+          {/* <ToiletList/> */}
+          {/* <UserList /> */}
+          {/* <AssetList/> */}
+        </div>
         <br />
         <br />
         <br />
         <FooterTab />
       </Container>
-
-
     </>
-  )
+  );
 }
